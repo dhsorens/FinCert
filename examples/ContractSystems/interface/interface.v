090188eebeb754674e54836d19c374fc2c4c7b39
Derek Sorensen <derekhsorensen@gmail.com>
2023-08-14 22:53:28 -0600

    interface example update

--- examples/ContractSystems/interface/interface.v
+++ examples/ContractSystems/interface/interface.v
@@ -8,6 +8,7 @@ From ConCert.Execution Require Import ChainedList.
 From ConCert.Utils Require Import RecordUpdate.
 From ConCert.Utils Require Import Extras.
 From Coq Require Import FunctionalExtensionality.
+From Coq Require Import ProofIrrelevance.
 From Coq Require Import Ensembles.
 From Coq Require Import ZArith_base.
 From Coq Require Import QArith_base.
@@ -50,7 +51,7 @@ Definition error := nat.
 Definition setup_i := Address.
 Inductive entrypoint_i := 
 | incr_i (n : nat).
-Record state_i := build_state_i { backend_address : Address }.
+Record state_i := build_state_i  { backend_address : Address }.
 Definition error_i := nat.
 
 (* for the backend *)
@@ -161,8 +162,8 @@ End Backend.
 End Modular.
 
 (* the systems in question *)
-Definition mono := singleton_sys contract_mono.
-Definition modu := nest contract_interface contract_backend.
+Definition mono_place := singleton_place_graph contract_mono.
+Definition modu_place := nest contract_interface contract_backend.
 
 
 Section Bisimulation.
@@ -178,48 +179,105 @@ Definition permissioned_ctx ctx := {|
 
 Section LinkGraph.
 
-(* The link graph specification for mono *)
-Axiom mono_link_graph_clnil : forall st (step : SystemStep mono st st),
-    step = clnil.
+(* The link graph specification for mono : steps are simply single system steps *)
+Definition mono_link st1 st2 := SingleSystemStep mono_place st1 st2.
 
-Axiom mono_link_graph_step : forall st1 st2 (step : SystemStep mono st1 st2),
-    exists c ctx n recv_ok,
-    let step_mono := build_sys_single_step mono st1 st2
-        c
-        ctx
-        (Some (incr n))
-        nil
-        recv_ok in 
-    step = snoc clnil step_mono.
+Definition mono_link_semantics st1 st2 (step : mono_link st1 st2) :
+    ChainedSingleSteps mono_place st1 st2 := 
+    (snoc clnil step).
 
-(* The link graph specification for modu *)
-Axiom modu_link_graph_clnil : forall st (step : SystemStep modu st st),
-    step = clnil.
+Definition mono_sys := {|
+    sys_place := mono_place ;
+    sys_link := mono_link ;
+    sys_link_semantics := mono_link_semantics ; 
+|}.
 
-Axiom modu_link_graph_step : forall st1 st2 (step : SystemStep modu st1 st2),
-    exists c ctx n recvi_ok recvb_ok, 
-    let step_interface := build_sys_single_step modu st1 st2 
-        c
-        ctx
-        (Some (inl (incr_i n))) 
-        [act_call backend_caddr 0 (serialize (incr_b n))]
-        recvi_ok in 
-    let step_backend := build_sys_single_step modu st1 st2
-        c
-        (permissioned_ctx ctx)
-        (Some (inr (incr_b n)))
-        nil
-        recvb_ok in 
-    step = snoc (snoc clnil step_interface) step_backend.
+
+(* the link graph specification for modu : steps are a pair of interface/backend calls *)
+Inductive modu_link st1 st2 := 
+|   step_incr c ctx n
+        (interface_ok : sys_receive modu_place c ctx st1 (Some (inl (incr_i n))) = 
+                        Ok (st1, [act_call backend_caddr 0 (serialize (incr_b n))]))
+        (backend_ok : sys_receive modu_place c (permissioned_ctx ctx) st1 (Some (inr (incr_b n))) =
+                        Ok (st2, nil)).
+
+Definition modu_link_semantics st1 st2 (step : modu_link st1 st2) :
+    ChainedSingleSteps modu_place st1 st2.
+Proof.
+    destruct step.
+    assert (SingleSystemStep modu_place st1 st1) as call_interface.
+    {   apply (build_sys_single_step modu_place st1 st1 c ctx (Some (inl (incr_i n)))
+                [act_call backend_caddr 0 (serialize (incr_b n))] interface_ok). }
+    assert (SingleSystemStep modu_place st1 st2) as call_backend.
+    {   apply (build_sys_single_step modu_place st1 st2 c (permissioned_ctx ctx) (Some (inr (incr_b n)))
+                nil backend_ok). }
+    apply (snoc (snoc clnil call_interface) call_backend).
+Defined.
+
+Definition modu_sys := {|
+    sys_place := modu_place ;
+    sys_link := modu_link ;
+    sys_link_semantics := modu_link_semantics ;
+|}.
 
 End LinkGraph.
 
 
 (* the morphisms *)
 (* mono -> modu *)
-Definition stm_mono_mod : SystemTraceMorphism mono modu.
+Definition st_mono_modu st := (build_state_i backend_caddr, build_state_b interface_caddr (counter st)).
+
+Lemma gen_mono_modu : forall init_sys_state,
+    is_genesis_sys_state mono_sys init_sys_state -> 
+    is_genesis_sys_state modu_sys (st_mono_modu init_sys_state).
 Proof.
-    apply (build_st_morph mono modu
+    intros st gen_mono.
+    unfold is_genesis_sys_state in *.
+    destruct gen_mono as [c [ctx [s init_ok]]].
+    exists c, ctx, (backend_caddr, interface_caddr).
+    unfold sys_init, modu_sys, mono_sys in *.
+    cbn in *.
+    unfold init in init_ok.
+    now inversion init_ok.
+Qed.
+
+Definition step_mono_modu st1 st2 (step : SystemStep mono_sys st1 st2) :
+    SystemStep modu_sys (st_mono_modu st1) (st_mono_modu st2).
+Proof.
+    destruct step, sys_step_msg.
+    2:{ unfold mono_place in *. 
+        cbn in *.
+        inversion sys_recv_ok_step. }
+    destruct e.
+    apply (step_incr 
+        ({| backend_address := backend_caddr |}, 
+            {| interface_address := interface_caddr; counter_backend := counter st1 |})
+        ({| backend_address := backend_caddr |}, 
+            {| interface_address := interface_caddr; counter_backend := counter st2 |})
+        sys_step_chain
+        sys_step_ctx
+        n).
+    +   auto.
+    +   unfold modu_place.
+        cbn.
+        unfold receive_b, permissioned_ctx.
+        cbn.
+        rewrite (address_eq_refl interface_caddr).
+        do 4 f_equal.
+        unfold mono_place in sys_recv_ok_step.
+        cbn in sys_recv_ok_step.
+        now inversion sys_recv_ok_step.
+Defined.
+
+Definition stm_mono_modu : SystemTraceMorphism mono_sys modu_sys := {|
+    st_state_morph := st_mono_modu ;
+    sys_genesis_fixpoint := gen_mono_modu ;
+    sys_step_morph := step_mono_modu ;
+|}.
+
+Definition stm_mono_modu' : SystemTraceMorphism mono_sys modu_sys.
+Proof.
+    apply (build_st_morph mono_sys modu_sys
         (* the function between state types *)
         (fun st =>
             (build_state_i backend_caddr, build_state_b interface_caddr (counter st)))).
@@ -227,53 +285,45 @@ Proof.
         unfold is_genesis_sys_state in *.
         destruct gen_mono as [c [ctx [s init_ok]]].
         exists c, ctx, (backend_caddr, interface_caddr).
-        unfold sys_init, modu, mono in *.
+        unfold sys_init, modu_sys, mono_sys in *.
         cbn in *.
         unfold init in init_ok.
         now inversion init_ok.
     -   intros st1 st2 step.
-        induction step; try apply clnil.
-        destruct l as [c ctx op_msg nacts recv_ok].
-        (* send step to *)
-        destruct op_msg.
-        2:{ (* none case *)
-            now unfold sys_receive in *. }
+        destruct step, sys_step_msg.
+        2:{ unfold mono_place in *. 
+            cbn in *.
+            inversion sys_recv_ok_step. }
         destruct e.
-        (* send the single step of the monolithic contract to a *pair* of steps
-            of the modular contract system *)
-        assert (SingleSystemStep modu 
+        apply (step_incr 
             ({| backend_address := backend_caddr |}, 
-                {| interface_address := interface_caddr; counter_backend := counter mid |})
+                {| interface_address := interface_caddr; counter_backend := counter st1 |})
             ({| backend_address := backend_caddr |}, 
-                {| interface_address := interface_caddr; counter_backend := counter mid |}))
-        as step_interface.
-        (* first the call to the interface contract, 
-            which results in a call to the backend contract *)
-        {   apply (build_sys_single_step modu _ _ c ctx 
-                (Some (inl (incr_i n))) [act_call backend_caddr 0 (serialize (incr_b n))]).
-            now unfold sys_receive in *. }
-        apply (snoc (snoc IHstep step_interface)).
-        clear step_interface.
-        (* then the call to the backend *)
-        apply (build_sys_single_step modu _ _ c (permissioned_ctx ctx)
-            (Some (inr (incr_b n))) nil).
-        unfold sys_receive, mono, modu in *.
-        cbn in *.
-        unfold receive_b.
-        cbn.
-        rewrite (address_eq_refl interface_caddr).
-        now inversion recv_ok.
+                {| interface_address := interface_caddr; counter_backend := counter st2 |})
+            sys_step_chain
+            sys_step_ctx
+            n).
+        +   auto.
+        +   unfold modu_place.
+            cbn.
+            unfold receive_b, permissioned_ctx.
+            cbn.
+            rewrite (address_eq_refl interface_caddr).
+            do 4 f_equal.
+            unfold mono_place in sys_recv_ok_step.
+            cbn in sys_recv_ok_step.
+            now inversion sys_recv_ok_step.
 Defined.
 
 
 (* modu -> mono *)
-Definition stm_mod_mono : SystemTraceMorphism modu mono.
+Definition stm_modu_mono : SystemTraceMorphism modu_sys mono_sys.
 Proof.
-    apply (build_st_morph modu mono
+    apply (build_st_morph modu_sys mono_sys
         (* the function between state types *)
         (fun '(st_i, st_b) => build_state (counter_backend st_b))).
     -   intros st gen_modu.
-        unfold is_genesis_sys_state, mono, modu in *.
+        unfold is_genesis_sys_state, mono_sys, modu_sys in *.
         destruct gen_modu as [c [ctx [s init_ok]]].
         exists c, ctx, tt.
         destruct st as [st_i st_b].
@@ -281,85 +331,114 @@ Proof.
         cbn in *.
         unfold sys_init, nest, init in *.
         now inversion init_ok.
-    -   intros st1 st2.
-        assert (forall P : (SystemStep modu st1 st2) -> Type,
-        forall c ctx n recvi_ok recvb_ok,
-        P (snoc (snoc clnil
-            (build_sys_single_step modu st1 st1 c ctx (Some (inl (incr_i n)))
-                [act_call backend_caddr 0 (serialize (incr_b n))]
-                recvi_ok))
-            (build_sys_single_step modu st1 st2 c (permissioned_ctx ctx)
-                (Some (inr (incr_b n))) nil
-                recvb_ok)) -> 
-        forall (step : SystemStep modu st1 st2), P step)
-        as H_modu_ind.
-        { admit. }
-        apply H_modu_ind.
-    
-        step.
-        (* by the link graph, step is either clnil or a nested pair of calls *)
-        induction step. 
-        +   admit.
-        +   
+    -   intros * step.
+        destruct sys_state1 as [st1_i st1_b].
+        destruct sys_state2 as [st2_i st2_b].
+        destruct step.
+        (* just need to build a single system step *)
+        apply (build_sys_single_step mono_place 
+            {| counter := counter_backend st1_b |} {| counter := counter_backend st2_b |}
+            c ctx (Some (incr n)) nil).
+        unfold mono_place, modu_place, permissioned_ctx in *.
+        cbn in *.
+        unfold receive_b in *.
+        cbn in *.
+        destruct (interface_caddr =? interface_address st1_b)%address; 
+        now inversion backend_ok.
+Defined.
 
-        assert Chain as c. { admit. }
-        assert ContractCallContext as ctx. { admit. } 
-        assert nat as n. { admit. }
-        assert (sys_receive modu c ctx st1 (Some (inl (incr_i n))) = 
-        Ok (st1, [act_call backend_caddr 0 (serialize (incr_b n))]))
-        as recvi_ok. { admit. }
-        assert (sys_receive modu c (permissioned_ctx ctx) st1 (Some (inr (incr_b n))) = Ok (st2, []))
-        as recvb_ok. { admit. }
-        assert (step =
-        snoc
-          (snoc clnil
-             {|
-               sys_step_chain := c;
-               sys_step_ctx := ctx;
-               sys_step_msg := Some (inl (incr_i n));
-               sys_step_nacts := [act_call backend_caddr 0 (serialize (incr_b n))];
-               sys_recv_ok_step := recvi_ok
-             |})
-          {|
-            sys_step_chain := c;
-            sys_step_ctx := permissioned_ctx ctx;
-            sys_step_msg := Some (inr (incr_b n));
-            sys_step_nacts := [];
-            sys_recv_ok_step := recvb_ok
-          |})
-        as step_eq. { admit. }
+Theorem mono_modu_bisim :
+    systems_bisimilar mono_sys modu_sys.
+Proof.
+    unfold systems_bisimilar.
+    exists stm_mono_modu, stm_modu_mono.
+    unfold is_iso_stm.
+    unfold compose_stm, id_stm.
+    split.
+    (* mono -> mono = id *)
+    -   assert (sys_step_compose stm_modu_mono stm_mono_modu = id_sys_step_morph mono_sys) 
+        as H_steps_eq.
+        {   apply functional_extensionality_dep.
+            intro st1.
+            apply functional_extensionality_dep.
+            intro st2.
+            apply functional_extensionality_dep.
+            intro step.
+            unfold id_sys_step_morph, sys_step_compose.
+            destruct step.
+            destruct sys_step_msg;
+            cbn.
+            +   destruct e.
+                pose sys_recv_ok_step as sys_recv_ok2.
+                cbn in sys_recv_ok2.
+                inversion sys_recv_ok2.
+                subst.
+                apply f_equal.
+                apply proof_irrelevance.
+            +   unfold sys_receive, mono_place in sys_recv_ok_step.
+                cbn in sys_recv_ok_step.
+                inversion sys_recv_ok_step. }
+        rewrite (eq_stm_dep mono_sys mono_sys 
+            (Basics.compose (st_state_morph modu_sys mono_sys stm_modu_mono) (st_state_morph mono_sys modu_sys stm_mono_modu))
+            (sys_genesis_compose stm_modu_mono stm_mono_modu)
+            (id_sys_genesis_fixpoint mono_sys)
+            (sys_step_compose stm_modu_mono stm_mono_modu)
+            (id_sys_step_morph mono_sys)
+            H_steps_eq).
+        now apply f_equal.
+    (* modu -> modu = id *)
+    -    assert (Basics.compose (st_state_morph mono_sys modu_sys stm_mono_modu) 
+                    (st_state_morph modu_sys mono_sys stm_modu_mono) = id)
+        as H_compose_eq.
+        {   apply functional_extensionality.
+            intro st.
+            destruct st as [st_i st_b].
+            cbn.
+            f_equal.
+            -   pose proof (H_sti st_i).
+                destruct st_i.
+                cbn in *.
+                now rewrite H.
+            -   pose proof (H_stb st_b).
+                destruct st_b.
+                cbn in *.
+                now rewrite H. }
+
+        apply (eq_stm_dep modu_sys modu_sys).
+            (Basics.compose (st_state_morph mono_sys modu_sys stm_mono_modu) (st_state_morph modu_sys mono_sys stm_modu_mono))).
+            (sys_genesis_compose stm_mono_modu stm_modu_mono)).
+        replace ((Basics.compose (st_state_morph mono_sys modu_sys stm_mono_modu)
+        (st_state_morph modu_sys mono_sys stm_modu_mono) init_sys_state))
+        with (id init_sys_state).
+
+
+        assert (Basics.compose (st_state_morph mono_sys modu_sys stm_mono_modu) (st_state_morph modu_sys mono_sys stm_modu_mono) = id)
+        as H_compose_eq.
+        {   apply functional_extensionality.
+            intro st.
+            destruct st as [st_i st_b].
+            cbn.
+            f_equal.
+            -   pose proof (H_sti st_i).
+                destruct st_i.
+                cbn in *.
+                now rewrite H.
+            -   pose proof (H_stb st_b).
+                destruct st_b.
+                cbn in *.
+                now rewrite H. }
         
+        unfold sys_step_compose. cbn. subst.
         
-        assert (exists c ctx n recvi_ok recvb_ok, 
-            step = snoc (snoc clnil 
-            (build_sys_single_step modu st1 st1
-            c
-            ctx
-            (Some (inl (incr_i n))) 
-            [act_call backend_caddr 0 (serialize (incr_b n))]
-            recvi_ok)) 
-            (build_sys_single_step modu st1 st2
-            c
-            (permissioned_ctx ctx)
-            (Some (inr (incr_b n)))
-            nil
-            recvb_ok)).
-        { admit. }
+        pose proof (eq_stm_dep modu_sys modu_sys 
+            (Basics.compose (st_state_morph mono_sys modu_sys stm_mono_modu) (st_state_morph modu_sys mono_sys stm_modu_mono))
+            (sys_genesis_compose stm_mono_modu stm_modu_mono)
+            (id_sys_genesis_fixpoint modu_sys)
+            ).
 
-        apply (snoc IHstep).
-        clear IHstep.
-        destruct l as [c ctx op_msg nacts recv_ok].
-        destruct mid as [st_i_mid st_b_mid].
-        destruct to  as [st_i_to  st_b_to].
-        +   admit.
-Admitted.
-
-(* assuming the semantics of the link graph are 
-    satisfied, ...
-    
-    these systems are bisimilar. *)
 
 
 End Bisimulation.
+
 
 End Interface.
