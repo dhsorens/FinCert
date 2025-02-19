5aba480b5560f98133c9e7a2684cc2489d0caf41
Derek Sorensen <dhsorens@Dereks-MacBook-Pro.local>
2025-02-19 16:31:01 -0500

    documentation

--- contracts/StructuredPools/StructuredPools.v
+++ contracts/StructuredPools/StructuredPools.v
@@ -3,6 +3,9 @@
 * Specification: https://ieeexplore.ieee.org/abstract/document/10174866
 * Paper associated with this: 
     Sorensen, D. (In)Correct Smart Contract Specifications. ICBC 2024.
+    Sorensen, D. Structured Pools for Tokenized Carbon Credits. ICBC 2023.
+    Jaffer, S., Dales, M., Ferris, P., Sorensen, D., Swinfield, T., Message, R., Keshav, S., and Madhavapeddy, A. Global, robust and comparable digital carbon assets. ICBC 2024.
+    Sorensen, D. Tokenized Carbon Credits. Ledger, 2023.
 *)
 
 (* import the specification of a structured pool and an FA2 contract *)
