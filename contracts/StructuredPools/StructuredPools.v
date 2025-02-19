52359d28f3c8b245b75153fa8c762b710e6cb162
Derek Sorensen <derek-certora@Dereks-MacBook-Pro.local>
2025-02-19 13:28:59 -0500

    pool entrypoint implemented and proved correct

--- contracts/StructuredPools/StructuredPools.v
+++ contracts/StructuredPools/StructuredPools.v
@@ -7,7 +7,7 @@
 
 (* import the specification of a structured pool and an FA2 contract *)
 From FinCert.Specifications Require Import StructuredPoolsSpec.
-From FinCert.Specifications.FA2Spec Require FA2Spec.
+From FinCert.Specifications.FA2Spec Require Import FA2Spec.
 From FinCert.Meta Require Import ContractMorphisms.
 From ConCert.Execution Require Import Blockchain.
 From ConCert.Execution Require Import Containers.
@@ -83,55 +83,77 @@ Definition init (_ : Chain) (_ : ContractCallContext) (n : setup) :
 
 (* entrypoint function definitions *)
 Definition pool_entrypoint 
-    (ctx: ContractCallContext) (st : state) (p : pool_data) : result := 
+    (ctx: ContractCallContext) (cstate : state) (msg_payload : pool_data) : result := 
     (* check that the token has an exchange rate *)
-    match FMap.find (token_pooled p) (stor_rates st)
+    match FMap.find (token_pooled msg_payload) (stor_rates cstate)
     with | None => Err 0 | Some r_x =>
     (* tokens_held goes up appropriately *)
-    let token := token_pooled p in
-    let qty := qty_pooled p in
-    let old_bal := get_bal token (stor_tokens_held st) in
-    let st' := FMap.add token (old_bal + qty) (stor_tokens_held st) in
+    let token := token_pooled msg_payload in
+    let qty := qty_pooled msg_payload in
+    let old_bal := get_bal token (stor_tokens_held cstate) in
+    let cstate' := FMap.add token (old_bal + qty) (stor_tokens_held cstate) in
     (* outstanding tokens change appropriately *)
-    let stor_outstanding_tokens' := stor_outstanding_tokens st + r_x * qty in
+    let stor_outstanding_tokens' := stor_outstanding_tokens cstate + r_x * qty in
     (* emit a TRANSFER call to the token in storage *)
-    let transfer_data := FA2Spec.Build_transfer_data 
-        ctx.(ctx_origin)
-        [ 
-            FA2Spec.Build_transfer_to 
-                p.(token_pooled).(token_address)
-                p.(token_pooled).(token_id)
-                qty
-        ] in
-    let act_transfer := 
-        act_call (p.(token_pooled).(token_address)) 0 
-        (serialize (FA2Spec.Transfer [ transfer_data ])) in
+    let transfer_to := {| 
+        to_ := msg_payload.(token_pooled).(token_address) ;
+        transfer_token_id := msg_payload.(token_pooled).(token_id) ;
+        amount := msg_payload.(qty_pooled) ; |} in 
+    let transfer_data := {| 
+        from_ := ctx.(ctx_from) ;
+        txs := [ transfer_to ] ; |} in
+    let transfer_payload := [ transfer_data ] in 
+    let transfer_call := act_call
+        (* calls the pooled token address *)
+        (msg_payload.(token_pooled).(token_address))
+        (* with amount 0 *)
+        0 
+        (* and payload transfer_payload *)
+        (serialize (Transfer transfer_payload)) in
     (* emit a MINT call *)
-    let mint_data := FA2Spec.Build_mint_data
-        ctx.(ctx_origin)
-        st.(stor_pool_token).(token_id)
-        qty in 
-    let mint_payload := (serialize (FA2Spec.Mint [ mint_data ])) in
-    let act_mint := act_call (st.(stor_pool_token).(token_address)) 0 mint_payload in 
+    let r_x := get_rate msg_payload.(token_pooled) (stor_rates cstate) in 
+    let mint_data := {|
+        mint_owner := ctx.(ctx_from) ;
+        mint_token_id := cstate.(stor_pool_token).(token_id) ;
+        qty := msg_payload.(qty_pooled) * r_x ; |} in 
+    let mint_payload := [ mint_data ] in
+    let mint_call := act_call 
+        (* calls the pool token contract *)
+        ((stor_pool_token cstate).(token_address))
+        (* with amount 0 *)
+        0
+        (* and payload mint_payload *)
+        (serialize (FA2Spec.Mint mint_payload)) in 
+    (* the state gets renewed *)
     let new_state := {|
-        stor_rates := st.(stor_rates) ;
-        stor_tokens_held := st' ;
-        stor_pool_token := st.(stor_pool_token) ;
+        stor_rates := cstate.(stor_rates) ;
+        stor_tokens_held := cstate' ;
+        stor_pool_token := cstate.(stor_pool_token) ;
         stor_outstanding_tokens := stor_outstanding_tokens' ; 
     |} in
-    Ok (new_state, [act_transfer ; act_mint ])
+    (* entrypoint ends *)
+    Ok (new_state, [ transfer_call ; mint_call ])
     end.
 
 Definition calc_rx_inv (r_x : N) (q : N) : N := todo.
 
 Definition unpool_entrypoint
-    (ctx: ContractCallContext) (st : state) (u : unpool_data) : result := 
+    (ctx: ContractCallContext) (cstate : state) (msg_payload : unpool_data) :
+    result := 
     (* checks for an exchange rate *)
+    match FMap.find msg_payload.(token_unpooled) (stor_rates cstate)
+    with | None => Err 0 | Some r_x =>
     (* tokens_held goes down appropriately *)
+    let token := msg_payload.(token_unpooled) in
+    let r_x := get_rate token (stor_rates cstate) in 
+    let qty := calc_rx_inv r_x msg_payload.(qty_unpooled) in
+    let old_bal := get_bal token (stor_tokens_held cstate) in 
+        (* let new_bal := get_bal token (stor_tokens_held cstate') in  *)
     (* outstanding tokens change appropriately *)
     (* rates don't change *)
     (* emits a BURN call to the pool_token in storage *)
-    todo.
+    todo
+    end.
 
 (* lots *)
 Definition calc_delta_y (rate_in : N) (rate_out : N) (qty_trade : N) (k : N) (x : N) : N := todo.
@@ -140,7 +162,7 @@ Definition calc_delta_y (rate_in : N) (rate_out : N) (qty_trade : N) (k : N) (x 
 Definition calc_rx' (rate_in : N) (rate_out : N) (qty_trade : N) (d : N) (x : N) : N := todo.
 
 Definition trade_entrypoint
-    (ctx: ContractCallContext) (st : state) (t : trade_data) : result := 
+    (ctx: ContractCallContext) (cstate : state) (msg_payload : trade_data) : result := 
     (* check that token_in and token_out have exchange rates *)
     (* check inputs to the trade calculation are all positive *)
     (* trade is priced calc_delta_y appropriately *)
@@ -175,6 +197,24 @@ Instance setup_spec : Setup_Spec setup := todo.
 Instance state_spec : State_Spec state := todo.
 Instance error_spec : Error_Spec error := todo.
 
+Axiom pool_types : forall p, pool p = StructuredPoolsSpec.pool p.
+Axiom unpool_types : forall u, unpool u = StructuredPoolsSpec.unpool u.
+Axiom trade_types : forall t, trade t = StructuredPoolsSpec.trade t.
+Axiom stor_rates_types : forall cstate, stor_rates cstate = StructuredPoolsSpec.stor_rates cstate.
+Axiom stor_pool_token_types : forall cstate, stor_pool_token cstate = StructuredPoolsSpec.stor_pool_token cstate.
+Axiom stor_tokens_held_types : forall cstate, stor_tokens_held cstate = StructuredPoolsSpec.stor_tokens_held cstate.
+Axiom stor_outstanding_tokens_types : forall cstate, stor_outstanding_tokens cstate = StructuredPoolsSpec.stor_outstanding_tokens cstate.
+
+Lemma fmap_find_add : forall (m : FMap token N) k v, 
+    FMap.find k (FMap.add k v m) = Some v.
+Proof. auto. Qed.
+
+Lemma fmap_add_neq : forall (m : FMap token N) k1 k2 v, 
+    k1 <> k2 -> FMap.find k1 (FMap.add k2 v m) = FMap.find k1 m.
+Proof. auto. Qed.
+
+(* the Structured Pools implementation is correct *)
+
 (* a proof that the Structured Pools implementation is correct *)  
 
 (* the Structured Pools implementation is correct *)
@@ -197,25 +237,117 @@ Proof.
         intros.
         destruct m eqn:H_m.
         +   left. exists p.
-            admit.
+            apply pool_types.
         +   right. left. exists u.
-            admit.
+            apply unpool_types.
         +   right. right. left. exists t.
-            admit.
+            apply trade_types.
     (* pool entrypoint spec *)
     -   unfold pool_entrypoint_check.
         intros * H_recv_ok.
-        admit.
+        simpl in H_recv_ok.
+        rewrite <- pool_types in H_recv_ok.
+        unfold pool_entrypoint in H_recv_ok.
+        destruct (FMap.find (token_pooled msg_payload) (stor_rates cstate)) 
+        as [r_x | r_x] eqn:H_rx;
+        try inversion H_recv_ok.
+        exists r_x.
+        now rewrite <- stor_rates_types.
     -   unfold pool_emits_txns.
-        admit.
-    -   admit.
-    -   admit.
+        intros * H_recv_ok.
+        simpl in H_recv_ok.
+        rewrite <- pool_types in H_recv_ok.
+        unfold pool_entrypoint in H_recv_ok.
+        destruct (FMap.find (token_pooled msg_payload) (stor_rates cstate)) 
+        as [r_x | r_x] eqn:H_rx;
+        try inversion H_recv_ok.
+        exists ({|
+            to_ := token_address (token_pooled msg_payload);
+            transfer_token_id := token_id (token_pooled msg_payload);
+            amount := qty_pooled msg_payload
+          |}).
+        exists ({|
+            from_ := ctx_from ctx;
+            txs :=
+              [{|
+                 to_ := token_address (token_pooled msg_payload);
+                 transfer_token_id := token_id (token_pooled msg_payload);
+                 amount := qty_pooled msg_payload
+               |}]
+          |}).
+        exists ([{|
+            from_ := ctx_from ctx;
+            txs :=
+              [{|
+                 to_ := token_address (token_pooled msg_payload);
+                 transfer_token_id := token_id (token_pooled msg_payload);
+                 amount := qty_pooled msg_payload
+               |}]
+          |}]).
+        exists ({|
+            mint_owner := ctx_from ctx;
+            mint_token_id := token_id (stor_pool_token cstate);
+            qty :=
+              qty_pooled msg_payload *
+                get_rate (token_pooled msg_payload) (stor_rates cstate)
+          |}).
+        exists ([{|
+            mint_owner := ctx_from ctx;
+            mint_token_id := token_id (stor_pool_token cstate);
+            qty := qty_pooled msg_payload *
+                get_rate (token_pooled msg_payload) (stor_rates cstate)
+          |}]).
+        rewrite <- stor_rates_types.
+        rewrite <- stor_pool_token_types.
+        repeat split; try now cbn.
+    -   rename H into H_recv_ok.
+        simpl in H_recv_ok.
+        rewrite <- pool_types in H_recv_ok.
+        do 2 rewrite <- stor_tokens_held_types.
+        unfold pool_entrypoint in H_recv_ok.
+        destruct (FMap.find (token_pooled msg_payload) (stor_rates cstate)) 
+        as [r_x | r_x] eqn:H_rx;
+        try inversion H_recv_ok.
+        unfold get_bal. cbn.
+        now rewrite fmap_find_add.
+    -   rename H into H_recv_ok.
+        simpl in H_recv_ok.
+        rewrite <- pool_types in H_recv_ok.
+        do 2 rewrite <- stor_tokens_held_types.
+        unfold pool_entrypoint in H_recv_ok.
+        destruct (FMap.find (token_pooled msg_payload) (stor_rates cstate)) 
+        as [r_x | r_x] eqn:H_rx;
+        try inversion H_recv_ok.
+        intros t H_t_neq.
+        cbn.
+        pose proof (fmap_add_neq (stor_tokens_held cstate) t (token_pooled msg_payload)
+        (get_bal (token_pooled msg_payload) (stor_tokens_held cstate) + qty_pooled msg_payload)
+        H_t_neq) as H_unchanged.
+        unfold get_bal in *.
+        now rewrite H_unchanged.
     -   unfold pool_rates_unchanged.
         intros * H_recv_ok t.
-        admit.
+        simpl in H_recv_ok.
+        rewrite <- pool_types in H_recv_ok.
+        do 2 rewrite <- stor_rates_types.
+        unfold pool_entrypoint in H_recv_ok.
+        destruct (FMap.find (token_pooled msg_payload) (stor_rates cstate)) 
+        as [r_x | r_x] eqn:H_rx;
+        now try inversion H_recv_ok.
     -   unfold pool_outstanding.
         intros * H_recv_ok.
-        admit.
+        simpl in H_recv_ok.
+        rewrite <- pool_types in H_recv_ok.
+        do 2 rewrite <- stor_outstanding_tokens_types.
+        unfold pool_entrypoint in H_recv_ok.
+        destruct (FMap.find (token_pooled msg_payload) (stor_rates cstate)) 
+        as [r_x | r_x] eqn:H_rx;
+        try inversion H_recv_ok.
+        cbn.
+        rewrite <- stor_rates_types.
+        unfold get_rate.
+        now replace (FMap.find (token_pooled msg_payload) (stor_rates cstate))
+        with (Some r_x) by now rewrite H_rx.
     (* unpool entrypoint spec *)
     -   unfold unpool_entrypoint_check.
         intros * H_recv_ok.
